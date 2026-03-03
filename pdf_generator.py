97131111da37233b7c80e8ae78f55b2d01d21cb0
Peter Eckert <hadukenduhund1234@gmail.com>
2026-03-03 19:05:17 +0100

    test

--- pdf_generator.py
+++ pdf_generator.py
@@ -1526,7 +1526,7 @@ class PDFGenerator(FPDF):
             self.ln(4); self.set_x(PAGE_L)
             self.set_font(self.fn, "", 7.5); self.set_text_color(*self.TEXT_LIGHT)
             self.multi_cell(0, 4.5, invoice_data["notes"], 0, "L")
-        pdf_raw  = self.output()
+        pdf_raw = self.output(dest='S').encode('latin-1') if isinstance(self.output(dest='S'), str) else self.output(dest='S')
         xml_data = self._build_xml(invoice_data, receiver_data, items, sender_data)
         return self._embed_xml(pdf_raw, xml_data), xml_data
 
